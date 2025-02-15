cdc58ff3d25904ad83ddfcc8e31d821679a610b4
00-aa <firekinggamerz542@gmail.com>
2025-02-15 19:39:59 +0530

    Add files via upload

---
+++ AUTH/E-MAIL/main.js
@@ -0,0 +1,18 @@
+  /*
+  let GoogleAuthButton = document.getElementById('main_auth_google');
+  let FacebookAuthButton = document.getElementById('main_auth_facebook');
+  let AppleAuthButton = document.getElementById('main_auth_apple');
+  
+  import { AuthEmailGoogle, CurrentUser, OnAuthChange } from '/firebase.js';
+  
+  GoogleAuthButton.onclick = AuthEmailGoogle;
+  FacebookAuthButton.onclick = AuthEmailGoogle;
+  AppleAuthButton.onclick = AuthEmailGoogle;
+  
+  OnAuthChange((User)=>{
+    if(User){
+      window.location.href = '/AUTH/PHONE/index.html';
+    }else{
+      //User Not Found
+    }
+  })
