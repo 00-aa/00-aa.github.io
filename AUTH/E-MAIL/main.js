30a5ac371bf37cd0a9094520195737a093dccf52
00-aa <firekinggamerz542@gmail.com>
2025-03-14 14:03:55 +0530

    Add files via upload

--- AUTH/E-MAIL/main.js
+++ AUTH/E-MAIL/main.js
@@ -1,17 +1,18 @@
-  let GoogleAuthButton = document.getElementById('main_auth_google');
-  let FacebookAuthButton = document.getElementById('main_auth_facebook');
-  let AppleAuthButton = document.getElementById('main_auth_apple');
+  import { AuthEmailGoogle, OnAuthChange } from '/firebase.js';
   
-  import { AuthEmailGoogle, CurrentUser, OnAuthChange } from '/firebase.js';
+  let GoogleAuthButton = document.getElementById('btn_auth_google');
+  let FacebookAuthButton = document.getElementById('btn_auth_facebook');
+  let AppleAuthButton = document.getElementById('btn_auth_apple');
   
   GoogleAuthButton.onclick = AuthEmailGoogle;
   FacebookAuthButton.onclick = AuthEmailGoogle;
   AppleAuthButton.onclick = AuthEmailGoogle;
   
-  OnAuthChange((User)=>{
-    if(User){
-      window.location.href = '/AUTH/PHONE/index.html';
-    }else{
-      //User Not Found
+  OnAuthChange((CurrUser)=>{
+    if(CurrUser){
+      window.location.href = '/HOME/index.html';
     }
-  })
+  });
+  
+  
+  
