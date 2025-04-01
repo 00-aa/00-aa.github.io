9e4139baf6222f87a96dcd74862623753a5a3d43
00-aa <firekinggamerz542@gmail.com>
2025-04-01 12:20:15 +0530

    Add files via upload

---
+++ HOME/main.js
@@ -0,0 +1,13 @@
+  import { OnChildAdded } from '/firebase.js';
+  
+  function Init(){
+    OnChildAdded('APP/BANNERS',(BannerImgData)=>{
+      let Banner = BannerImgData.val();
+      banner.innerHTML += `
+        <img loading="lazy" src="${Banner.ImageUrl}" id="banner_img" name="${BannerImgData.key}">
+      `;
+    });
+    
+  }
+  
+  addEventListener('load',Init)
